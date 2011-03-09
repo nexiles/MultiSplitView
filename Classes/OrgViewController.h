5b129861f47ceb1ee208e4a939b204e2584d8232
Stefan Eletzhofer <se@nexiles.de>
2011-03-09 20:10:56 +0100

    template

---
+++ Classes/OrgViewController.h
@@ -0,0 +1,16 @@
+//
+//  OrgViewController.h
+//  MultiSplitView
+//
+//  Created by Stephan Eletzhofer on 09.03.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OrgViewController : UITableViewController {
+
+}
+
+@end
