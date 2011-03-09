f9f0635ecf2c83a3b47bf721972230767e5f743c
Stefan Eletzhofer <se@nexiles.de>
2011-03-09 21:12:48 +0100

    Org Root VC

---
+++ Classes/OrgRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  OrgRootViewController.h
+//  MultiSplitView
+//
+//  Created by Stephan Eletzhofer on 09.03.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OrgRootViewController : UITableViewController {
+
+}
+
+@end
