f208543552d6a756e057d9167c8f0608afc7db43
Stefan Eletzhofer <se@nexiles.de>
2011-03-09 20:05:10 +0100

    view registry

--- Classes/ViewRegistry.h
+++ Classes/ViewRegistry.h
@@ -10,7 +10,16 @@
 
 
 @interface ViewRegistry : NSObject {
-
+	NSMutableDictionary *_registry;
 }
 
+@property (readonly) NSMutableDictionary *registry;
+
++(ViewRegistry *)sharedViewRegistry;
+
+-(void)registerViewController:(id)controller forName:(NSString *)name;
+-(id)controllerForName:(NSString *)name;
+
 @end
+
+// vim: set ts=2 sw=2 expandtab:
